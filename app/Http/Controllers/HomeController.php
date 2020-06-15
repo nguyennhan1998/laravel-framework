a38e1bc10c8ad296e985ce2b807be2b19e1c1e8b
Nguyen nhan <nguyennnhanlovemyself@gmail.com>
2020-06-15 10:44:05 +0700

    mail

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -33,9 +33,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $u=Auth::user();
-        $u->role=User::ADMIN_ROLE;
-        $u->save();
+//        $u=Auth::user();
+//        $u->role=User::ADMIN_ROLE;
+//        $u->save();
 
         if(!Cache::has("home_page")){
             $most_views = Product::orderBy("view_count", "DESC")->limit(8)->get();
